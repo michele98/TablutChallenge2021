04b642f6690ed3b6a91b916422ced65425153215
michele98 <calvanese.michele@gmail.com>
2021-11-23 16:12:11 +0100

    Improved console prints

--- src/it/unibo/ai/didattica/competition/tablut/becchi/player/PlayerBecco.java
+++ src/it/unibo/ai/didattica/competition/tablut/becchi/player/PlayerBecco.java
@@ -15,6 +15,8 @@ public abstract class PlayerBecco {
     private final State.Turn color;
     private final AdversarialSearch<State, Action> solver;
     private Action previousAction = null;
+    private double searchTime = 0.0;
+    private int movesMade = 0;
 
     public PlayerBecco(State.Turn color, int timeout, GameBecchiTablut game, Heuristic heuristic) {
         this.color = color;
@@ -27,6 +29,7 @@ public abstract class PlayerBecco {
 
     public Action getOptimalAction (State state) throws IOException {
         previousAction = solver.makeDecision(state);
+        movesMade++;
         return previousAction;
     }
 
@@ -34,7 +37,9 @@ public abstract class PlayerBecco {
         if (verbose) {
             System.out.println("Choosing optimal action...");
         }
+        double startTime = System.currentTimeMillis();
         Action result = getOptimalAction(state);
+        searchTime = (System.currentTimeMillis() - startTime);
         printMetrics();
 
         return result;
@@ -42,10 +47,12 @@ public abstract class PlayerBecco {
 
     public String getMetrics() {
         Metrics metrics = solver.getMetrics();
-        String message = "Search summary:";
+
+        String message = "Search summary for my move " + movesMade + ":";
         message += "\n - Maximum depth: " + metrics.get("maxDepth");
         message += "\n - Nodes expanded: " + metrics.get("nodesExpanded");
         message += "\n - Action chosen: " + previousAction.toString();
+        message += "\n - time used for search: " + searchTime + " ms";
         return message;
     }
 
